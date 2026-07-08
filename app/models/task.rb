0e3f478811b9218f3227d61f386d840c6c1926b1
root <root@DESKTOP-OLH42EM.localdomain>
2026-07-08 16:29:18 +0300

    Добавлен базовый функционал проектов: форма создания, контроллер и отображение (не протестировано)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,32 @@
 class Task < ApplicationRecord
   belongs_to :project
+
+  # 1. Проверяем, что статус берется строго из списка
+  validates :status, inclusion: { in: [ "To Do", "In Progress", "In Testing", "Rejected", "Done" ] }
+
+  # 2. Указываем Rails запустить нашу собственную проверку переходов
+  validate :check_status_transition
+
+  def check_status_transition
+    case status_was
+    when "Done"
+      errors.add(:status, "статус Done нельзя изменить")
+    when "To Do"
+      if status != "In Progress"
+        errors.add(:status, "статус To Do можно изменить только на In Progress")
+      end
+    when "In Progress"
+      if status != "In Testing" && status != "To Do"
+        errors.add(:status, "статус In Progress можно изменить только на In Testing или To Do")
+      end
+    when "In Testing"
+      if status != "Done" && status != "Rejected"
+        errors.add(:status, "статус In Testing можно изменить только на Done или Rejected")
+      end
+    when "Rejected"
+      if status != "In Progress"
+        errors.add(:status, "статус Rejected можно изменить только на In Progress")
+      end
+    end
+  end
 end
