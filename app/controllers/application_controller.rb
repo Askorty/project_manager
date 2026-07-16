fa9ec0736f1b01ba21c97c8a704d9ffdeb6d8d6a
root <root@DESKTOP-OLH42EM.localdomain>
2026-07-16 11:41:02 +0300

    Добавил обработку исключения RecordInvalid в ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,10 @@ class ApplicationController < ActionController::Base
   stale_when_importmap_changes
 
   rescue_from StandardError, with: :render_internal_error unless Rails.env.development?
+
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
 
   private
 
@@ -13,5 +16,9 @@ class ApplicationController < ActionController::Base
 
   def render_internal_error(exception)
     render file: Rails.root.join("public", "500.html"), status: :internal_server_error, layout: false
+  end
+
+  def record_invalid(exception)
+    redirect_back fallback_location: root_path, alert: "Ошибка сохранения: #{exception.message}"
   end
 end
